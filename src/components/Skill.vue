9c694ac953d0767e537d6d9266c11800effe200b
Jeffrey Walst <jeffreywalst@gmail.com>
2018-12-11 15:20:54 +0100

    Added the new skills look to portfolio

--- src/components/Skill.vue
+++ src/components/Skill.vue
@@ -1,154 +1,205 @@
 <template>
-  <div class="container mb-5" id="skills">
-    <div class>
-      <h1 class="text-center">Skills</h1>
+    <div class="container-fluid pt-2 pb-2 mb-5" id="skills">
+        <div class="back-image"></div>
+        <div class="container">
+            <div class="row mt-3">
+                <div class="col-md-12">
+                    <h1 class="text-center text-white">Skills</h1>
+                </div>
+                <div class="col-lg-12 col-md-12 col-12 mb-3 fa-pull-left">
+                    <div class="row under-icon mt-4">
+                        <div class="col-lg-12">
+                            <h3 class="text-center">Frameworks</h3>
+                        </div>
+                    </div>
+                    <div class="row mt-3">
+                        <div v-for="wd in languages" class="col-lg-4 col-md-4 col-12 fa-pull-left text-center mb-3">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <h5 class="">{{wd.exp}}</h5>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-3">
+                                    <font-awesome-icon
+                                            class="text-black text-center normal-icon align-middle"
+                                            size="4x"
+                                            :icon="[wd.type, wd.name]"
+                                    >
+                                    </font-awesome-icon>
+                                </div>
+                                <div class="col-9">
+                                    <div class="col-2 fa-pull-left align-middle p-0" v-for="ws in wd.stars">
+                                        <font-awesome-icon v-bind:class="{ checked: ws }"
+                                                           class="text-center w-75 align-middle "
+                                                           size="4x"
+                                                           :icon="['fas', 'star']"
+                                        ></font-awesome-icon>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
-      <div class="row mt-4">
-        <!-- MCV / CMS -->
-        <div class="col-lg-12 col-md-12 col-12 mb-md-5 mb-lg-0 mb-5 fa-pull-left">
-          <div class="row under-icon mt-4">
-            <div class="col-lg-12">
-              <h3 class="text-center">Frameworks</h3>
+                    <!-- Webdev -->
+                    <div
+                            class="col-lg-12 col-md-12 col-12 mt-3 mb-3 fa-pull-left"
+                    >
+                        <div class="row under-icon mt-4">
+                            <div class="col-lg-12 col-12 text-center">
+                                <h3 class>Webdevelopment</h3>
+                            </div>
+                        </div>
+                        <div class="row mt-3">
+                            <div v-for="wd in webdev" class="col-lg-4 col-md-4 col-12 fa-pull-left text-center mb-3">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <h5 class="">{{wd.exp}}</h5>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-3">
+                                        <font-awesome-icon
+                                                class="text-black text-center normal-icon align-middle"
+                                                size="4x"
+                                                :icon="[wd.type, wd.name]"
+                                        >
+                                        </font-awesome-icon>
+                                    </div>
+                                    <div class="col-9">
+                                        <div class="col-2 fa-pull-left align-middle p-0" v-for="ws in wd.stars">
+                                            <font-awesome-icon v-bind:class="{ checked: ws }"
+                                                               class="text-center w-75 align-middle "
+                                                               size="4x"
+                                                               :icon="['fas', 'star']"
+                                            ></font-awesome-icon>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Workflow -->
+                    <div class="col-lg-12 col-md-12 col-12 mt-3 mb-5 fa-pull-left">
+                        <div class="row under-icon">
+                            <div class="col-lg-12 col-12 text-center">
+                                <h3 class>Workflow</h3>
+                            </div>
+                        </div>
+                        <div class="row mt-3">
+                            <div v-for="wd in flow" class="col-lg-4 col-md-4 col-12 fa-pull-left text-center mb-3">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <h5 class="">{{wd.exp}}</h5>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-3">
+                                        <font-awesome-icon
+                                                class="text-black text-center normal-icon align-middle"
+                                                size="4x"
+                                                :icon="[wd.type, wd.name]"
+                                        >
+                                        </font-awesome-icon>
+                                    </div>
+                                    <div class="col-9">
+                                        <div class="col-2 fa-pull-left align-middle p-0" v-for="ws in wd.stars">
+                                            <font-awesome-icon v-bind:class="{ checked: ws }"
+                                                               class="text-center w-75 align-middle "
+                                                               size="4x"
+                                                               :icon="['fas', 'star']"
+                                            ></font-awesome-icon>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-          <div class="row">
-            <div v-for="wd in languages" class="col-lg-4 col-md-2 col-4 fa-pull-left text-center">
-              <font-awesome-icon
-                      class="text-black text-center normal-icon"
-                      size="4x"
-                      :icon="[wd.type, wd.name]"
-              ></font-awesome-icon>
-              <span class="stars " v-for="ws in wd.stars">
-                <font-awesome-icon v-bind:class="{ checked: ws }"
-                                   class="text-center"
-                                   size="1x"
-                                   :icon="['fas', 'star']"
-                ></font-awesome-icon>
-              </span>
-            </div>
-          </div>
         </div>
-
-        <!-- Webdev -->
-        <div
-          class="col-lg-12 col-md-12 col-12 mt-md-5 mb-md-5 mb-lg-0 mt-lg-0 mt-5 mb-5 fa-pull-left"
-        >
-          <div class="row under-icon mt-4">
-            <div class="col-lg-12 col-12 text-center">
-              <h3 class>Webdevelopment</h3>
-            </div>
-          </div>
-          <div class="row">
-            <div v-for="wd in webdev" class="col-lg-4 col-md-2 col-4 fa-pull-left text-center mb-3">
-                <font-awesome-icon
-                  class="text-black text-center normal-icon"
-                  size="4x"
-                  :icon="[wd.type, wd.name]"
-                ></font-awesome-icon>
-              <span class="stars " v-for="ws in wd.stars">
-                <font-awesome-icon v-bind:class="{ checked: ws }"
-                        class="text-center"
-                        size="1x"
-                        :icon="['fas', 'star']"
-                ></font-awesome-icon>
-              </span>
-            </div>
-          </div>
-        </div>
-
-        <!-- Workflow -->
-        <div class="col-lg-3 col-md-12 col-12 offset-lg-1 mt-sm-5 mt-lg-0 mt-5 fa-pull-left">
-          <div class="row under-icon mt-4">
-            <div class="col-lg-12">
-              <h3 class="text-center">Workflow</h3>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-lg-4 col-md-2 col-4 col-sm-4 fa-pull-left text-center offset-lg-0 offset-md-3">
-              <span class="fa-stack fa-lg fa-3x">
-                <font-awesome-icon class="fa-stack-2x" :icon="['fas', 'circle']"></font-awesome-icon>
-                <font-awesome-icon
-                  class="fa-stack-1x text-white text-center"
-                  :icon="['fab', 'linux']"
-                ></font-awesome-icon>
-              </span>
-            </div>
-            <div class="col-lg-4 col-md-2 col-4 fa-pull-left text-center">
-              <span class="fa-stack fa-lg fa-3x">
-                <font-awesome-icon class="fa-stack-2x" :icon="['fas', 'circle']"></font-awesome-icon>
-                <font-awesome-icon
-                  size="lg"
-                  class="fa-stack-1x text-white text-center"
-                  :icon="['fab', 'apple']"
-                ></font-awesome-icon>
-              </span>
-            </div>
-            <div class="col-lg-4 col-md-2 col-4 fa-pull-left text-center">
-              <span class="fa-stack fa-lg fa-3x">
-                <font-awesome-icon class="fa-stack-2x" :icon="['fas', 'circle']"></font-awesome-icon>
-                <font-awesome-icon
-                  class="fa-stack-1x text-white text-center"
-                  :icon="['fab', 'gitlab']"
-                ></font-awesome-icon>
-              </span>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
-  </div>
+    </div>
 </template>
 
 <script>
-export default {
-  name: "Skill",
-  data: function () {
-    return {
-      languages: [
-        {
-          name: "drupal",
-          type: "fab",
-          stars: ['checked', 'checked', 'checked', 'checked', 'checked']
-        },
-        {
-          name: "laravel",
-          type: "fab",
-          stars: ['checked', 'checked', 'checked', 'checked', 'checked']
-        },
-        {
-          name: "vuejs",
-          type: "fab",
-          stars: ['checked', 'checked', 'checked', '', '']
+    export default {
+        name: "Skill",
+        data: function () {
+            return {
+                languages: [
+                    {
+                        name: "drupal",
+                        exp: "Drupal 7/8",
+                        type: "fab",
+                        stars: ['checked', 'checked', 'checked', 'checked', null]
+                    },
+                    {
+                        name: "laravel",
+                        exp: "Laravel 5.x",
+                        type: "fab",
+                        stars: ['checked', 'checked', 'checked', 'checked', null]
+                    },
+                    {
+                        name: "vuejs",
+                        exp: "VueJS Cli 3",
+                        type: "fab",
+                        stars: ['checked', 'checked', 'checked', null, null]
+                    }
+                ],
+                webdev: [
+                    {
+                        name: "html5",
+                        exp: "HTML",
+                        type: "fab",
+                        stars: ['checked', 'checked', 'checked', 'checked', 'checked']
+                    },
+                    {
+                        name: "css3",
+                        exp: "CSS",
+                        type: "fab",
+                        stars: ['checked', 'checked', 'checked', 'checked', 'checked']
+                    },
+                    {
+                        name: "php",
+                        type: "fab",
+                        exp: "PHP 7",
+                        stars: ['checked', 'checked', 'checked', 'checked', null]
+                    },
+                    {
+                        name: "database",
+                        exp: "MySQL Database",
+                        type: "fas",
+                        stars: ['checked', 'checked', 'checked', 'checked', null]
+                    },
+                    {
+                        name: "js",
+                        exp: "Javascript",
+                        type: "fab",
+                        stars: ['checked', 'checked', 'checked', null, null]
+                    },
+                ],
+                flow: [
+                    {
+                        name: "linux",
+                        exp: "Linux Server",
+                        type: "fab",
+                        stars: ['checked', 'checked', 'checked', 'checked', 'checked']
+                    },
+                    {
+                        name: "apple",
+                        exp: "Apple Mac OS",
+                        type: "fab",
+                        stars: ['checked', 'checked', 'checked', 'checked', 'checked']
+                    },
+                    {
+                        name: "git",
+                        exp: "Git",
+                        type: "fab",
+                        stars: ['checked', 'checked', 'checked', 'checked', null]
+                    }
+                ]
+            }
         }
-      ],
-      webdev: [
-        {
-          name: "php",
-          type: "fab",
-          stars: ['checked', 'checked', 'checked', 'checked', '']
-        },
-        {
-          name: "html5",
-          type: "fab",
-          stars: ['checked', 'checked', 'checked', 'checked', 'checked']
-        },
-        {
-          name: "database",
-          type: "fas",
-          stars: ['checked', 'checked', 'checked', 'checked', '']
-        },
-        {
-          name: "css3",
-          type: "fab",
-          stars: ['checked', 'checked', 'checked', 'checked', 'checked']
-        },
-        {
-          name: "js",
-          type: "fab",
-          stars: ['checked', 'checked', 'checked', 'checked', '']
-        },
-      ]
-    }
-  }
-};
+    };
 </script>
