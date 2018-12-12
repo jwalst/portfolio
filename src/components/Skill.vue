a590940281f4b27b62e3cf45682df959f1bb1023
Jeffrey Walst <jeffreywalst@gmail.com>
2018-12-12 13:15:17 +0100

    Fixed binds on for loops

--- src/components/Skill.vue
+++ src/components/Skill.vue
@@ -1,19 +1,19 @@
 <template>
-    <div class="container-fluid pt-2 pb-2 mb-5" id="skills">
+    <div class="container-fluid pt-2 pb-2 mb-2" id="skills">
         <div class="back-image"></div>
         <div class="container">
             <div class="row mt-3">
                 <div class="col-md-12">
                     <h1 class="text-center text-white">Skills</h1>
                 </div>
-                <div class="col-lg-12 col-md-12 col-12 mb-3 fa-pull-left">
+                <div class="col-lg-12 col-md-12 col-12 mb-1 fa-pull-left">
                     <div class="row under-icon mt-4">
                         <div class="col-lg-12">
                             <h3 class="text-center">Frameworks</h3>
                         </div>
                     </div>
                     <div class="row mt-3">
-                        <div v-for="wd in languages" class="col-lg-4 col-md-4 col-12 fa-pull-left text-center mb-3">
+                        <div v-bind:key="wd.id" v-for="wd in languages" class="col-lg-4 col-md-4 col-12 fa-pull-left text-center mb-3">
                             <div class="row">
                                 <div class="col-md-12">
                                     <h5 class="">{{wd.exp}}</h5>
@@ -29,7 +29,7 @@
                                     </font-awesome-icon>
                                 </div>
                                 <div class="col-9">
-                                    <div class="col-2 fa-pull-left align-middle p-0" v-for="ws in wd.stars">
+                                    <div class="col-2 fa-pull-left align-middle p-0" v-bind:key="ws" v-for="ws in wd.stars">
                                         <font-awesome-icon v-bind:class="{ checked: ws }"
                                                            class="text-center w-75 align-middle "
                                                            size="4x"
@@ -51,7 +51,7 @@
                             </div>
                         </div>
                         <div class="row mt-3">
-                            <div v-for="wd in webdev" class="col-lg-4 col-md-4 col-12 fa-pull-left text-center mb-3">
+                            <div v-bind:key="wd.id" v-for="wd in webdev" class="col-lg-4 col-md-4 col-12 fa-pull-left text-center mb-3">
                                 <div class="row">
                                     <div class="col-md-12">
                                         <h5 class="">{{wd.exp}}</h5>
@@ -67,7 +67,7 @@
                                         </font-awesome-icon>
                                     </div>
                                     <div class="col-9">
-                                        <div class="col-2 fa-pull-left align-middle p-0" v-for="ws in wd.stars">
+                                        <div class="col-2 fa-pull-left align-middle p-0" v-bind:key="ws" v-for="ws in wd.stars">
                                             <font-awesome-icon v-bind:class="{ checked: ws }"
                                                                class="text-center w-75 align-middle "
                                                                size="4x"
@@ -80,14 +80,14 @@
                         </div>
                     </div>
                     <!-- Workflow -->
-                    <div class="col-lg-12 col-md-12 col-12 mt-3 mb-5 fa-pull-left">
+                    <div class="col-lg-12 col-md-12 col-12 mt-3 fa-pull-left">
                         <div class="row under-icon">
                             <div class="col-lg-12 col-12 text-center">
                                 <h3 class>Workflow</h3>
                             </div>
                         </div>
                         <div class="row mt-3">
-                            <div v-for="wd in flow" class="col-lg-4 col-md-4 col-12 fa-pull-left text-center mb-3">
+                            <div v-bind:key="wd.id" v-for="wd of flow" class="col-lg-4 col-md-4 col-12 fa-pull-left text-center mb-3">
                                 <div class="row">
                                     <div class="col-md-12">
                                         <h5 class="">{{wd.exp}}</h5>
@@ -103,7 +103,7 @@
                                         </font-awesome-icon>
                                     </div>
                                     <div class="col-9">
-                                        <div class="col-2 fa-pull-left align-middle p-0" v-for="ws in wd.stars">
+                                        <div class="col-2 fa-pull-left align-middle p-0" v-bind:key="ws" v-for="ws in wd.stars">
                                             <font-awesome-icon v-bind:class="{ checked: ws }"
                                                                class="text-center w-75 align-middle "
                                                                size="4x"
@@ -119,7 +119,6 @@
             </div>
         </div>
     </div>
-    </div>
 </template>
 
 <script>
@@ -129,18 +128,21 @@
             return {
                 languages: [
                     {
+                        id: 1,
                         name: "drupal",
                         exp: "Drupal 7/8",
                         type: "fab",
                         stars: ['checked', 'checked', 'checked', 'checked', null]
                     },
                     {
+                        id: 2,
                         name: "laravel",
                         exp: "Laravel 5.x",
                         type: "fab",
                         stars: ['checked', 'checked', 'checked', 'checked', null]
                     },
                     {
+                        id: 3,
                         name: "vuejs",
                         exp: "VueJS Cli 3",
                         type: "fab",
@@ -149,30 +151,35 @@
                 ],
                 webdev: [
                     {
+                        id: 1,
                         name: "html5",
                         exp: "HTML",
                         type: "fab",
                         stars: ['checked', 'checked', 'checked', 'checked', 'checked']
                     },
                     {
+                        id: 2,
                         name: "css3",
                         exp: "CSS",
                         type: "fab",
                         stars: ['checked', 'checked', 'checked', 'checked', 'checked']
                     },
                     {
+                        id: 3,
                         name: "php",
                         type: "fab",
                         exp: "PHP 7",
                         stars: ['checked', 'checked', 'checked', 'checked', null]
                     },
                     {
+                        id: 4,
                         name: "database",
                         exp: "MySQL Database",
                         type: "fas",
                         stars: ['checked', 'checked', 'checked', 'checked', null]
                     },
                     {
+                        id: 5,
                         name: "js",
                         exp: "Javascript",
                         type: "fab",
@@ -181,18 +188,21 @@
                 ],
                 flow: [
                     {
+                        id: 1,
                         name: "linux",
                         exp: "Linux Server",
                         type: "fab",
                         stars: ['checked', 'checked', 'checked', 'checked', 'checked']
                     },
                     {
+                        id: 2,
                         name: "apple",
                         exp: "Apple Mac OS",
                         type: "fab",
                         stars: ['checked', 'checked', 'checked', 'checked', 'checked']
                     },
                     {
+                        id: 3,
                         name: "git",
                         exp: "Git",
                         type: "fab",
